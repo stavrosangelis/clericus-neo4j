30e9eeab9ab6377e92383fc8d7f3d5bca2d619e0
Stavros <stavrosangelis@gmail.com>
2020-05-27 19:33:27 +0100

    minor bug fix

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -364,10 +364,10 @@ const temporalEvents = async(params) => {
   if (dateType==="after") {
     operator=">";
   }
-  if (startDate==="") {
+  if (startDate==="" || startDate=null || startDate="null") {
     return [];
   }
-  if (dateType==="range" && endDate==="") {
+  if (dateType==="range" && ((startDate==="" || startDate=null || startDate="null" || endDate==="" || endDate=null || endDate="null")) {
     return [];
   }
 
