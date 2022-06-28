811efbb1bc4006da2b7b3297d98c498d0fc72be5
Stavros <stavrosangelis@gmail.com>
2022-06-28 10:25:29 +0100

    Updated app.js and db-driver for better ssl support

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: '.env.production' });
+const { NODE_ENV } = process.env;
+// load the appropriate environment variables
+let envPath;
+switch (NODE_ENV) {
+  case 'production':
+    envPath = '.env.production';
+    break;
+  case 'development':
+    envPath = '.env.development';
+    break;
+  case 'development.ssl':
+    envPath = '.env.development.ssl';
+    break;
+  case 'devserver':
+    envPath = '.env.develserver';
+    break;
+  default:
+    envPath = '.env.production';
+    break;
 }
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
-}
-if (process.env.NODE_ENV === 'development.ssl') {
-  require('dotenv').config({ path: '.env.development.ssl' });
-}
-if (process.env.NODE_ENV === 'devserver') {
-  require('dotenv').config({ path: '.env.develserver' });
-}
+require('dotenv').config({ path: envPath });
+
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
@@ -17,31 +27,34 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-// const bodyParser = require('body-parser')
 const api = require('./routes/api');
-const port = process.env.PORT;
+
+const {
+  ARCHIVEPATH,
+  PORT: port,
+  SERVERURL,
+  RESOURCESPATH,
+  UPLOADSPATH,
+} = process.env;
+
 const app = express();
 app.use(compression());
-
 app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(
   express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000 })
 );
-
 app.use('/api', api);
-// serve files from archive
-app.use(express.static(process.env.ARCHIVEPATH));
-// serve resources
-app.use(express.static(process.env.RESOURCESPATH));
-// serve uploads
-app.use(express.static(process.env.UPLOADSPATH));
-// set json & file limit
-// app.use(bodyParser.json({limit: '50mb',extended: true}));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000}));
 
-// add https support
-if (process.env.NODE_ENV === 'development.ssl') {
+// serve files from archive
+app.use(express.static(ARCHIVEPATH));
+// serve resources
+app.use(express.static(RESOURCESPATH));
+// serve uploads
+app.use(express.static(UPLOADSPATH));
+
+// add https support and start server
+if (NODE_ENV === 'development.ssl') {
   const key = fs.readFileSync(
     path.resolve(__dirname, './cert/localhost-key.pem'),
     'utf8'
@@ -54,14 +67,13 @@ if (process.env.NODE_ENV === 'development.ssl') {
   const options = {
     key: key,
     cert: cert,
-    // passphrase: 'localhost',
   };
   /** start express server */
   https.createServer(options, app).listen(port, () => {
-    console.log(`HTTPS-enabled server started at port: ${port}`);
+    console.log(`HTTPS-enabled server started at: ${SERVERURL}api`);
   });
 } else {
   http.createServer(app).listen(port, () => {
-    console.log(`Server started at port: ${port}`);
+    console.log(`Server started at: ${SERVERURL}api`);
   });
 }
--- src/config/db-driver.js
+++ src/config/db-driver.js
@@ -1,13 +1,16 @@
 const neo4j = require('neo4j-driver');
 const path = require('path');
 
-const { DB_URI, DB_USER, DB_PASS, SSL_CERT_PATH } = process.env;
-
-const config = {
-  encrypted: 'ENCRYPTION_ON',
-  trust: 'TRUST_ALL_CERTIFICATES',
-  trustedCertificates: [path.resolve(__dirname, SSL_CERT_PATH)],
-};
+const { DB_URI, DB_USER, DB_PASS, SSL_CERT_PATH, NODE_ENV } = process.env;
+const hasSSL = ['development.ssl', 'production'];
+const config =
+  hasSSL.indexOf(NODE_ENV) > -1
+    ? {
+        encrypted: 'ENCRYPTION_ON',
+        trust: 'TRUST_ALL_CERTIFICATES',
+        trustedCertificates: [path.resolve(__dirname, SSL_CERT_PATH)],
+      }
+    : {};
 const driver = neo4j.driver(DB_URI, neo4j.auth.basic(DB_USER, DB_PASS), config);
 
 module.exports = driver;
